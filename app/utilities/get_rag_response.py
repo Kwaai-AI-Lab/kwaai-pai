febd346710f81018253260e05c0cc9a5543286d7
Ana Pacheco <144283863+AnaArkus@users.noreply.github.com>
2024-02-14 17:27:54 -0700

    add rag_handler

---
+++ app/utilities/get_rag_response.py
@@ -0,0 +1,14 @@
+import logging
+import os
+import warnings
+from embedchain import App
+from utilities.config import MODEL_RAG
+from utilities.rag_handler import RAGHandler
+
+
+
+def get_rag_response(question) -> str:
+    rag_manager = RAGHandler(MODEL_RAG)
+    response = rag_manager.get_rag_response(question)
+    
+    return response
---
+++ app/utilities/rag_handler.py
@@ -0,0 +1,52 @@
+import os
+import logging
+import warnings
+from utilities.config import HUGGINGFACE_ACCESS_TOKEN, MODEL_RAG
+from embedchain import App
+
+
+os.environ["HUGGINGFACE_ACCESS_TOKEN"] = HUGGINGFACE_ACCESS_TOKEN
+
+class RAGHandler:
+    _instance = None
+    
+    def __new__(cls, model_config):
+        if cls._instance is None:
+            cls._instance = super(RAGHandler, cls).__new__(cls)
+            cls._instance._rag_app = App.from_config(model_config)
+        return cls._instance
+    
+    # def __init__(self, model_config) -> None:
+    #     if RAGHandler._rag_app is None:  
+    #         RAGHandler._rag_app = App.from_config(model_config)  
+
+    @property
+    def rag_app(self):
+        return self._rag_app
+
+    
+    def get_rag_response(self,question) -> str:
+        """
+        Function to execute a query and log the response.
+
+        Args:
+            question (str): The query to be executed.
+
+        Returns:
+            str: The response from the query execution.
+        """
+        try:
+            logging.info(f"Executing query: {question}")
+
+            with warnings.catch_warnings():
+                warnings.filterwarnings("ignore", category=FutureWarning)
+                response = self.rag_app.query(question)
+                logging.info(f"Response: {response} \n")
+
+                return response
+        except Exception as e:
+            logging.exception("Unexpected error occurred when checking unseen emails.")
+            return ({"detail": " An unexpected error occurred, " + str(e)}) 
+        
+    def add_source(self):        
+        self.rag_app.add('utilities/emails.csv')
