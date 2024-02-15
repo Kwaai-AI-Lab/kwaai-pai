b99c287c0b4d8ef54d27832b9b46bcb82c75e185
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-02-14 18:41:16 -0600

    fix: fix ./run.sh file

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 
 tmux new-session -d -s llava_session 'cd quantization/executables && ./llava.llamafile -m ../models/Publisher/Repository/model_tuned_q8_0.gguf'
 
-# sleep 5
+sleep 5
 
-
-# docker-compose up --build
+docker-compose up --build
