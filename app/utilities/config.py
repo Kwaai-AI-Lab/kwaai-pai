a2be46d2c6ea3838efe80f758f10c98b374ec906
Ana Pacheco <144283863+AnaArkus@users.noreply.github.com>
2024-02-15 09:54:38 -0700

    add config

--- app/utilities/config.py
+++ app/utilities/config.py
@@ -11,3 +11,46 @@ EMAIL_CREDENTIALS = {
 MODEL_SPAM_FILTER = "NotShrirang/albert-spam-filter"
 API_KEY_OPENAI = "sk-****"
 MODEL_TAGGING = "gpt-3.5-turbo"
+
+HUGGINGFACE_ACCESS_TOKEN = "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"
+MODEL_RAG = "utilities/mistral.yaml"
+RAG_SOURCE = "utilities/emails.csv"
+
+# App config using OpenAI gpt-3.5-turbo-1106 as LLM
+EC_APP_CONFIG = {
+    "app": {
+        "config": {
+            "id": "embedchain-demo-app",
+        }
+    },
+    "llm": {
+        "provider": "openai",
+        "config": {
+            "model": "gpt-3.5-turbo-1106",
+        }
+    }
+}
+
+# Uncomment this configuration to use Mistral as LLM
+# EC_APP_CONFIG = {
+#     "app": {
+#         "config": {
+#             "name": "embedchain-opensource-app"
+#         }
+#     },
+#     "llm": {
+#         "provider": "huggingface",
+#         "config": {
+#             "model": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+#             "temperature": 0.1,
+#             "max_tokens": 250,
+#             "top_p": 0.1
+#         }
+#     },
+#     "embedder": {
+#         "provider": "huggingface",
+#         "config": {
+#             "model": "sentence-transformers/all-mpnet-base-v2"
+#         }
+#     }
+# }
