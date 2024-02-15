8808994d38a3d3f20de3e74bb1128f0851bda84d
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-02-14 18:40:33 -0600

    fix: update llamafile session command

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-tmux new-session -d -s llava_session 'cd quantization/executables && ./llava.llamafile'
+tmux new-session -d -s llava_session 'cd quantization/executables && ./llava.llamafile -m ../models/Publisher/Repository/model_tuned_q8_0.gguf'
 
-sleep 5
+# sleep 5
 
-docker-compose up --build
+
+# docker-compose up --build
