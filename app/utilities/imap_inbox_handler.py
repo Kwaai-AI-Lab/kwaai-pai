026c9d2771f0c32cdd94c3b82fefe07f4271a1ad
Ana Pacheco <144283863+AnaArkus@users.noreply.github.com>
2024-02-29 14:15:34 -0700

    Add hugging face token variable

--- app/utilities/imap_inbox_handler.py
+++ app/utilities/imap_inbox_handler.py
@@ -9,13 +9,13 @@ import torch.nn.functional as F
 import logging
 from rest_framework.response import Response
 from rest_framework import status, generics
-from utilities.config import MODEL_SPAM_FILTER, API_KEY_OPENAI, MODEL_TAGGING
+from utilities.config import MODEL_SPAM_FILTER, OPENAI_API_KEY, MODEL_TAGGING
 import pandas as pd
 
 from openai import OpenAI
 import os
 
-CLIENT = OpenAI(api_key= API_KEY_OPENAI)
+CLIENT = OpenAI(api_key= OPENAI_API_KEY)
 
 class ImapInboxHandler:
     def __init__(
