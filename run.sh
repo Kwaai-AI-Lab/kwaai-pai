22724b008cdb44c4f3ca07033d8e249e51166936
jrauljperez07 <jrauljperez02.dev@gmail.com>
2024-03-11 11:26:27 -0600

    work: work in progress

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tmux new-session -d -s llava_session 'cd quantization && ./llava.llamafile -m ./merged_adapters/ggml-model-q8_0.gguf'
+tmux new-session -d -s llava_session 'cd quantization && chmod +x llava.llamafile && ./llava.llamafile -m ./merged_adapters/ggml-model-q8_0.gguf'
 
 sleep 5
 
